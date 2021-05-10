d23d7d217d82493da03df562f04d9c63277ce6ab
Sonic <sf7293gv@go.minnstate.edu>
2021-05-10 06:10:06 -0500

    DB setup

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let api_routes = require('./routes/api.js')
+let path = require('path')
 
 // Create web app
 let app = express();
+
+let vueClientPath = path.join(__dirname, 'student-sign-in-client', 'dist')
+app.use(express.static(vueClientPath))
 
 // be able to handle JSON requests and convert data to javascript
 app.use(express.json())
